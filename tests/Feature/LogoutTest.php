05d51bc67806675ff800c62a0cef03f8ec2c1ca6
unknown <elisabet.v.alvarez@gmail.com>
2022-07-26 20:25:53 +0200

    changes in LogoutTest

--- tests/Feature/LogoutTest.php
+++ tests/Feature/LogoutTest.php
@@ -10,28 +10,20 @@ use Laravel\Passport\Passport;
 
 class LogoutTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 
     public function testAnAuthenticatedUserCanLogout()
-{
+    {
+
     $this->artisan('passport:install');
 
     $user = User::factory()->create();
 
+    
     Passport::actingAs($user);
 
-    $this->postJson('api/logout')
-        ->assertStatus(200);
-}
+    $response = $this->postJson('api/logout');
 
+    $response->assertStatus(200);
+
+    }
 }
